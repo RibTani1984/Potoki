6e285d0c4f6db1ebcc5678b52052f279e6dcf8cb
Tanya Ribakova <tanya2021rib@gmail.com>
2023-05-22 20:25:35 +0300

    serial

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,20 @@ import java.util.stream.IntStream;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        File file = new File("basket.txt");
+        File file = new File("basket.bin");
         String[] products = {"Батон", "Сырники", "Пельмени", "Мука", "Творог"};
         int[] prices = {50, 220, 280, 80, 90};
 
         Basket basket = null;
         if (file.exists()) {
-            basket = new Basket(products, prices);
+            basket = Basket.loadFromBinFile(file);
         } else {
-            basket = Basket.loadFromTxtFile(file);
+            basket = new Basket(products, prices);
         }
         System.out.println("Список возможных товаров для покупки:");
         IntStream
                 .range(0, products.length)
                 .forEach(i1 -> System.out.println((i1 + 1) + " " + products[i1] + ":  " + prices[i1] + " " + " руб/шт "));
-
         while (true) {
             System.out.println("Выберите необходимый товар и количество через пробел или введите 'end'");
             String line = scanner.nextLine();
@@ -30,7 +29,7 @@ public class Main {
             int productNumber = Integer.parseInt(parts[0]) - 1;
             int productCont = Integer.parseInt(parts[1]);
             basket.addToCart(productNumber, productCont);
-            basket.saveTxt(file);
+            basket.saveBin(file);
         }
         basket.printCart();
     }
